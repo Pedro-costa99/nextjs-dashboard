c8830a9367480c90f50157d01850c8ceca8d0afe
Pedro Costa <pedro-ccosta@live.com>
2025-11-19 10:53:01 -0300

    Limpa codigo

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -21,7 +21,6 @@ export function UpdateInvoice({ id }: { id: string }) {
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
-      teste
     </Link>
   );
 }
